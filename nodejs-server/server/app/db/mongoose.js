fe6c8fc746751f08f90b9f5a35e4569223d492c9
amir gilboa <amirg76@gmail.com>
2022-06-26 10:49:07 +0300

    update mern files

--- nodejs-server/server/app/db/mongoose.js
+++ nodejs-server/server/app/db/mongoose.js
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+const URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.y7roq.mongodb.net/?retryWrites=true&w=majority`;
 
-mongoose.connect(
-  "mongodb+srv://amirg76:BaRS3JnIqbpUxxup@cluster0.y7roq.mongodb.net/?retryWrites=true&w=majority",
-  (error, mongoConnectionInstance) => {
-    if (error) throw Error("Mongoose Connection!!, Error: " + error);
-    if (!process.env.NODE_ENV) {
-      const { host, port, name } = mongoConnectionInstance;
-      console.log({ host, port, name });
-    }
+mongoose.connect(URL, (error, mongoConnectionInstance) => {
+  if (error) throw Error("Mongoose Connection!!, Error: " + error);
+  if (!process.env.NODE_ENV) {
+    const { host, port, name } = mongoConnectionInstance;
+    console.log({ host, port, name });
   }
-);
+});
 
 // mongoose.connect(
 //   "mongodb://127.0.0.1/Bank",
--- nodejs-server/server/server.js
+++ nodejs-server/server/server.js
@@ -165,6 +165,7 @@
 import { app } from "./app/app.js";
 import "./app/db/mongoose.js";
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log("SERVER IS UP AND RUNNING ON PORT " + PORT));
 
 // "heroku-postbuild": "npm install --prefix /client && npm run build --prefix /client"
